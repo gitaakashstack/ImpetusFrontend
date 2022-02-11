be719ed4783b64889f7ab182324f13a2ec013c84
Fauzan Mujtaba Siddiqui <86624307+FauzanMS@users.noreply.github.com>
2022-02-11 21:48:48 +0530

    Update Event2.js

--- src/EventsPage/Event2.js
+++ src/EventsPage/Event2.js
@@ -85,7 +85,7 @@ export default function Event2() {
         amount: data.amount.toString(),
         order_id: data.id,
         name: "Yantra Search",
-        description: "Thank you for nothing. Please give us some money",
+        description: "Thank you for registering.",
         image: `${process.env.REACT_APP_SERVER}/logo.svg`,
         handler: function (response) {
           if(response.razorpay_payment_id){
