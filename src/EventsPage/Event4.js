5c33dfe167872aaa8799b7d0b9543f03290e60b0
Fauzan Mujtaba Siddiqui <86624307+FauzanMS@users.noreply.github.com>
2022-02-11 21:50:06 +0530

    Update Event4.js

--- src/EventsPage/Event4.js
+++ src/EventsPage/Event4.js
@@ -87,7 +87,7 @@ export default function Event4() {
         amount: data.amount.toString(),
         order_id: data.id,
         name: "Trade Assemble",
-        description: "Thank you for nothing. Please give us some money",
+        description: "Thank you for registering.",
         image: `${process.env.REACT_APP_SERVER}/logo.svg`,
         handler: function (response) {
           if(response.razorpay_payment_id){
