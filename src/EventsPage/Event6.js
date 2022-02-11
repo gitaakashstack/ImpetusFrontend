5854a34d5f19ceadf01827b7588081864c589ef0
Fauzan Mujtaba Siddiqui <86624307+FauzanMS@users.noreply.github.com>
2022-02-11 21:51:15 +0530

    Update Event6.js

--- src/EventsPage/Event6.js
+++ src/EventsPage/Event6.js
@@ -87,7 +87,7 @@ export default function Event6() {
         amount: data.amount.toString(),
         order_id: data.id,
         name: "TrustMe",
-        description: "Thank you for nothing. Please give us some money",
+        description: "Thank you for registering.",
         image: `${process.env.REACT_APP_SERVER}/logo.svg`,
         handler: function (response) {
           if(response.razorpay_payment_id){
